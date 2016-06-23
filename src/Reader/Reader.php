20dc600870820d1a4df76acb7ebd2aeff7fbdebb
huyanping <hypxm@qq.com>
2016-06-23 14:47:13 +0800

    test

--- src/Reader/Reader.php
+++ src/Reader/Reader.php
@@ -43,7 +43,7 @@ class Reader extends AbstractReader
 
         $cwd = '/tmp';
         $this->process = proc_open($command, $descriptors, $this->pipes, $cwd);
-        //stream_set_blocking($this->pipes[1], false);
+        stream_set_blocking($this->pipes[1], false);
         $this->handle = $this->pipes[1];
     }
 
