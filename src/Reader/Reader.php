5c6a6339dcccd28ea48e90c4017b6df12af4f12f
huyanping <hypxm@qq.com>
2016-06-23 14:42:30 +0800

    test

--- src/Reader/Reader.php
+++ src/Reader/Reader.php
@@ -34,7 +34,7 @@ class Reader extends AbstractReader
      */
     public function open()
     {
-        $command = "tail -f {$this->file}";
+        $command = "tail -F {$this->file}";
         $descriptors = array(
             0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
             1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
@@ -44,6 +44,7 @@ class Reader extends AbstractReader
         $cwd = '/tmp';
         $this->process = proc_open($command, $descriptors, $this->pipes, $cwd);
         stream_set_blocking($this->pipes[1], false);
+        var_dump($this->pipes[1]);
         $this->handle = $this->pipes[1];
     }
 
