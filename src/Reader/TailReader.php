acc720849d28d7b603bbcd002660557447376f59
huyanping <hypxm@qq.com>
2016-07-06 17:48:56 +0800

    add scrutinizer.yml

--- src/Reader/TailReader.php
+++ src/Reader/TailReader.php
@@ -34,7 +34,7 @@ class TailReader implements ReaderInterface
         $descriptors = array(
             0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
             1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
-            2 => array("file", $this->error_file, "a") // stderr is a file to write to
+            2 => array("file", "/dev/null", "a") // stderr is a file to write to
         );
 
         $cwd = '/tmp';
